b067ef174a2e807c114a1716446f3ead44470503
Adrien <agsummitv@gmail.com>
2025-06-12 09:06:54 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  headers: async () => [
-    {
-      source: '/(.*)',
-      headers: [
-        {
-          key: 'Content-Security-Policy',
-          value: `
-            default-src 'self';
-            script-src 'self' 'sha256-Tzzh4ZNs/VztgIxDWej5V0cAL3JoGXekk5k5Z2oXB1I=' 'sha256-CO8tMRuqZiB0Bdam1OXMTBftqUUnap5wIS7acDXQVM8=';
-            style-src 'self' 'unsafe-inline';
-            img-src * data: blob:;
-            connect-src *;
-            font-src 'self';
-          `.replace(/\s{2,}/g, ' ').trim()
-        }
-      ],
-    },
-  ],
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' https://vercel.live;
+              connect-src 'self' https://vercel.live wss://*.pusher.com;
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https:;
+              font-src 'self' https:;
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
