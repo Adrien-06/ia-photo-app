137ed60cd43432e77bac43cf63de7e82dcfcfd58
Adrien <agsummitv@gmail.com>
2025-06-12 08:37:55 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: true,
-  },
   headers: async () => [
     {
       source: '/(.*)',
       headers: [
         {
           key: 'Content-Security-Policy',
-          value:
-            "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src * data: blob:; media-src *; connect-src *; font-src 'self';",
-        },
+          value: `
+            default-src 'self';
+            script-src 'self' 'sha256-Tzzh4ZNs/VztgIxDWej5V0cAL3JoGXekk5k5Z2oXB1I=' 'sha256-CO8tMRuqZiB0Bdam1OXMTBftqUUnap5wIS7acDXQVM8=';
+            style-src 'self' 'unsafe-inline';
+            img-src * data: blob:;
+            connect-src *;
+            font-src 'self';
+          `.replace(/\s{2,}/g, ' ').trim()
+        }
       ],
     },
   ],
