ad8764048442098a25d7e9e570104937f1517d55
Adrien <agsummitv@gmail.com>
2025-06-12 08:07:50 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // Appliquer la règle à toutes les routes
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "script-src 'self' 'unsafe-eval'; object-src 'none';"
-          }
-        ]
-      }
-    ];
-  }
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: true,
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
