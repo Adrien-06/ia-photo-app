96bf285e7a3b6112b8681b5d3f73b457c7eb71fc
Adrien <agsummitv@gmail.com>
2025-06-12 08:28:50 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
   compiler: {
     removeConsole: true,
   },
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Content-Security-Policy',
+          value:
+            "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src * data: blob:; media-src *; connect-src *; font-src 'self';",
+        },
+      ],
+    },
+  ],
 }
 
 module.exports = nextConfig
